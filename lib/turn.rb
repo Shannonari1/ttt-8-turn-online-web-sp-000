83a1d8e3ab1aa9b92f395a70591263d158a81cc6
Shannon Young <shannon.ari1@gmail.com>
2020-09-12 13:23:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,30 +1,10 @@
-def display_board(board)
-   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-   puts "-----------"
-   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-   puts "-----------"
-   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 def turn(board)
-    puts "Please enter 1-9:"
-    user_input = gets.chomp
-    index = input_to_index(user_input)
-    if !valid_move?(board, index)
-      turn(board)
-    else
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
       move(board, index, current_player(board))
-    end
-    display_board(board)
+      turn(board)
+   end
+   display_board(board)
 end
-
-def turn_count(board)
-  # counts occupied positions
-  count = 0
-  board.each do |index|
-    if index != " "
-      count += 1
-    end
-  end
-    count
- end
