3a91b74cdfa5884b74d66bc463f11d0f0f0879f3
Shannon Young <shannon.ari1@gmail.com>
2020-09-12 13:47:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,35 +1,45 @@
-def input_to_index(input)
-  input.to_i - 1
-end
-
-def valid_move?(input, board)
-  input?(0, 8) && !(position_taken?(board, input)
-end
-
-def move(array, index, name = "X")
-  array[index] = name
-end
-
-def position_taken?(board, answer)
-  board[answer] != " "
-end
-def display_board(board)
-   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
    puts "-----------"
-   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
    puts "-----------"
-   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
+ 
+ def input_to_index(user_input)
+   user_input.to_i - 1
+ end
+ 
+  def move(position, char)
+   @board[position] = char
+ end
 
-def turn(board)
-    puts "Please enter 1-9:"
-    answer = gets.chomp
-    answer = input_to_index(answer)
-    if valid_move?(answer, board)
-      move(board, answer)
-      puts display_board(board)
-    else
-      puts "That is an invalid entry!"
-      turn(board)
-    end
+ def position_taken?(index_i)
+   ((@board[index_i] == "X") || (@board[index_i] == "O"))
+ end
+
+ def valid_move?(index)
+   index.between?(0,8) && !position_taken?(index)
+ end
+ 
+ def turn_count
+   number_of_turns = 0
+   @board.each do |space|
+   if space == "X" || space == "O"
+       number_of_turns += 1
+   end
+ end
+ return number_of_turns
+ 
+def turn
+   puts "Please enter 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+   char = current_player
+   if valid_move?(index)
+     move(index, char)
+     display_board
+   else
+     turn
+   end
 end
